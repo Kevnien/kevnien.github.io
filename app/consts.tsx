645106239f786b619cf9f1455b2a7d6e423e0aae
Kev <kevinn858@gmail.com>
2025-07-09 17:02:40 -0700

    moved image srcs to local

--- app/consts.tsx
+++ app/consts.tsx
@@ -25,8 +25,7 @@ const CONSTS = {
     {
       title: "Newsletter Section",
       url: "https://kevnien.github.io/newsletter-section/",
-      thumbnailSrc:
-        "https://ucdf0fe8762c7105e52e649bfda3.previews.dropboxusercontent.com/p/thumb/ACq6HDtLbOtgOBskBRRosLwdmypLx1Zs1wBdI-_nis2EysHWugxtASXRXu4qoqXoQ9mhIt_VQRUJ3KMnEGYv5Uc-sEs4-8MaSxMtPjp2ElCYbZLxntjTCa4Zdr7ZqUGxIuq-9YUs0xxsuHi6T4o-2Rnc-_qdJzZ0slFosMwQAZ5RMn4vJUKMAiksQSnIeej5J4jOktAkeKAEdXsJrbQ2TcCogbqXHRVzUDHO9FXFhBdfJVABA56XrByQMoLySNzbHYIKHcc25KIv3g3fwjyd6EbGtC_Zh27B879wxeoOLXfVw8M9ZQlLnemVARrkhNntBYpw3pK1Dm0Q-6q28_CFI41vBwwKJ5yjL41CKqAy_7Y1AbTFZkFJQjnUJeeYPwkpIvo/p.png?is_prewarmed=true",
+      thumbnailSrc: "/newsletter-section-thumbnail.png",
       alt: "newsletter section thumbnail - bullet points, email input, subscribe button, and image of newsletter contents",
       date: new Date("June 27, 2025"),
       description:
@@ -35,8 +34,7 @@ const CONSTS = {
     {
       title: "Collections Grid",
       url: "https://kevnien.github.io/collections-grid-section/",
-      thumbnailSrc:
-        "https://uc3a25e76a6b39601d35ff45a139.previews.dropboxusercontent.com/p/thumb/ACql6QoEjGT4_blt7N0_bj10HJXSC5vAXRCnRlVYWyGtf8qicOczxJ1s_41PuQsOa_5Irf7XgEw-GgPndMSHos4SGveZKyYLGCiJ64Dc3xRUOSbTNrkrXCJh0CEP-sPqGr11ydHBbxXFYQq9P1IUaW9n1tg6UctkQr2cebFtTEohb7wP-TCohNSRl6JRoksnITqF4nttSPSP6yEx6HDoJWhuTxiRPxO4GXsUDckWY1-UPRhc4z9JVrWDNt15XaLYtXq92CKIBNUUXBRtkrITdm8XKW43Q2YaMyO7VayxDE6Vwbcs9IJhJLZPOIKCdbPFndb1YdwSVvzD1Vma53myCjGBnPGPgQYn2BSDgHtQvvDS8bCkyeQOpLWQCHdMAFnIFIw/p.png?is_prewarmed=true",
+      thumbnailSrc: "/collections-grid-thumbnail.png",
       alt: "collections grid thumbnail - grid of images with titles and descriptions",
       date: new Date("June 23, 2025"),
       description:
